3b17015e0c6a0bbd9232ddc543e5d6f7c4736f84
Jitul Teron <jitulteron9@gmail.com>
2021-01-08 15:22:03 +0530

    production lev cli

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const db =mongoose.connect("mongodb://localhost:27017/customercli",{
     useCreateIndex:true
 });
 
+// Add cusotmer
 const addCustomer= (customer)=>{
      Customer.create(customer).then(data=>{
          console.info(chalk.bold.black.bgGreen("New Customer added in the DataBase"));
@@ -16,14 +17,15 @@ const addCustomer= (customer)=>{
         process.exit();
      })
 }
-
+// Find cusotmer
 const findCustomer= (name)=>{
     let search=RegExp(name,"i");
     Customer.find({$or:[{firstName:search},{lastName:search}]}).then(data=>{
         if(data.length>0){
+            console.info(chalk.green.underline.bold("Results"));
             for(var i =0;i<data.length;i++){
-                console.info(chalk.green.underline.bold("Results"));
                 console.info(chalk.yellow("----------","Customer No",1+i,"----------"));
+                console.info(chalk.magenta.bold("First Name:"),chalk.blue(data[i]._id));
                 console.info(chalk.magenta.bold("First Name:"),chalk.blue(data[i].firstName));
                 console.info(chalk.magenta.bold("Last Name:"),chalk.blue(data[i].lastName));
                 console.info(chalk.magenta.bold("Phone Number:"),chalk.blue(data[i].phone));
@@ -42,4 +44,53 @@ const findCustomer= (name)=>{
 
 }
 
-module.exports={addCustomer,findCustomer};
+// Update Cutomer
+
+const UpdateCustomer=(_id,customer)=>{
+    Customer.updateOne({_id},customer)
+    .then(updates=>{
+        console.info(chalk.bgGreen.bold.white(`${customer.firstName} ${customer.lastName} Updated`));
+        mongoose.connection.close();
+        process.exit();
+    })
+    
+    
+}
+
+// delete Cutomer
+const DeleteCustomer=(_id)=>{
+
+    Customer.findByIdAndDelete({_id})
+    .then(customer=>{
+        console.info(chalk.bgGreen.bold.white(`$Cutomer ${customer._id} deleted`));
+        mongoose.connection.close();
+        process.exit();
+    })
+    
+    
+}
+
+// list of all the customer
+const CustomerList=()=>{
+
+    Customer.find()
+    .then(customer=>{
+        console.info(chalk.bold.green("--------List of Customer--------"));
+        for(var i=0;i<customer.length;i++){
+                console.info(chalk.yellow("----------",1+i,"----------"));
+                console.info(chalk.magenta.bold("First Name:"),chalk.blue(customer[i]._id));
+                console.info(chalk.magenta.bold("First Name:"),chalk.blue(customer[i].firstName));
+                console.info(chalk.magenta.bold("Last Name:"),chalk.blue(customer[i].lastName));
+                console.info(chalk.magenta.bold("Phone Number:"),chalk.blue(customer[i].phone));
+                console.info(chalk.magenta.bold("Email"),chalk.blue(customer[i].email));
+                
+        }
+
+        console.info(chalk.green.underline.bold("----------total ",customer.length,"----------"));
+        mongoose.connection.close();
+        process.exit();
+    })
+    
+    
+}
+module.exports={addCustomer,findCustomer,UpdateCustomer,DeleteCustomer,CustomerList};
--- commander.js
+++ commander.js
@@ -1,5 +1,6 @@
+#!/usr/bin/env node
 require("dotenv").config();
-const {addCustomer,findCustomer}=require("./app");
+const {addCustomer,findCustomer,UpdateCustomer,DeleteCustomer,CustomerList}=require("./app");
 let inquirer=require("inquirer");
 // read the doc
 const program=require("commander");
@@ -18,7 +19,7 @@ let quetions=[
     {
         type:"input",
         name:"phone",
-        message:"Customer Phone Name"
+        message:"Customer Phone Number"
     },
     {
         type:"input",
@@ -27,24 +28,61 @@ let quetions=[
     }
 ]
 
+let updateQuery=[
+    {
+        type:"input",
+        name:"_id",
+        message:"Customer ID"
+    },
+    {
+        type:"input",
+        name:"",
+        message:"Customer ID"
+    }
+];
 program
 .version('1.0.0')
 .description("CLI to manage CRUD operation");
-
+// note dont need inquier then add the .command like (add <firstName> <lastName> <phone> <email>)
+// if not then do like this 
 program
-.command('add <firstName> <lastName> <phone> <email>')
+.command('add')
 .alias('a')
 .description("to add customer details")
 .action(()=>{
     inquirer.prompt(quetions).then(answer=>addCustomer(answer))
-    .catch(e=>{console.log(e,"gdfgdf")});
 })
+
 program
 .command('find <name>')
 .alias('f')
 .description("to find the name in the database")
 .action((name)=>{
     findCustomer(name);
+})
+
+program
+.command('delete <_id>')
+.alias('d')
+.description("to find the name in the database")
+.action((_id)=>{
+   DeleteCustomer(_id)
+})
+
+program
+.command('update <_id>')
+.alias('up')
+.description("to update current user data")
+.action((_id)=>{
+    inquirer.prompt(quetions).then(answer=>UpdateCustomer(_id,answer));
+})
+
+program
+.command('list')
+.alias('l')
+.description("List of the customer")
+.action(()=>{
+    CustomerList();
 })
 // The process.argv property returns an array containing the command-line arguments passed when the Node.js process was launched
 program.parse(process.argv);
